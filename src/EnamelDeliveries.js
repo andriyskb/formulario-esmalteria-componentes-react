2030a573515d6fc61f88613bdea1dce3eb910360
andriyskb <148741323+andriyskb@users.noreply.github.com>
2024-04-16 14:58:43 +0000

    avanzando

--- src/EnamelDeliveries.js
+++ src/EnamelDeliveries.js
@@ -1,7 +1,60 @@
 import React from "react";
 
 function EnamelDeliveries() {
-    return <div>EnamelDeliveries</div>
+    return (<div className="py-2 h-screen">
+        <div className="text-2xl text-center text-neutral">BATEA</div>
+        <div className="flex h-1/4">
+            <div className="w-1/6 divider divider-horizontal divider-success">06:00</div>
+            <div className="flex-auto flex flex-col justify-center">
+                <div className="h-1/6 text-center font-semibold">
+                    <span>CICLO <span className="text-primary font-bold">1</span></span>
+                </div>
+                <div className="h-1/4 flex justify-around items-center space-x-2">
+                    <div className="text-center flex-1 btn btn-outline">
+                       RECIBIDO 
+                    </div>
+                     <div className="text-center flex-1 btn btn-outline">
+                       PROCESADO 
+                    </div>
+                </div>
+            </div>
+            <div className="w-1/6 divider divider-horizontal divider-success">09:45</div>
+        </div>
+        <div className="flex h-1/4">
+            <div className="w-1/6 divider divider-horizontal divider-success">09:45</div>
+            <div className="flex-auto flex flex-col justify-center">
+                <div className="h-1/6 text-center font-semibold">
+                    <span>CICLO <span className="text-primary font-bold">2</span></span>
+                </div>
+                <div className="h-1/4 flex justify-around items-center space-x-2">
+                    <div className="text-center flex-1 btn btn-outline">
+                       RECIBIDO 
+                    </div>
+                     <div className="text-center flex-1 btn btn-outline">
+                       PROCESADO 
+                    </div>
+                </div>
+            </div>
+            <div className="w-1/6 divider divider-horizontal divider-success">12:25</div>
+        </div>
+        <div className="flex h-1/4">
+            <div className="w-1/6 divider divider-horizontal divider-success">12:25</div>
+            <div className="flex-auto flex flex-col justify-center">
+                <div className="h-1/6 text-center font-semibold">
+                    <span>CICLO <span className="text-primary font-bold">3</span></span>
+                </div>
+                <div className="h-1/4 flex justify-around items-center space-x-2">
+                    <div className="text-center flex-1 btn btn-outline">
+                       RECIBIDO 
+                    </div>
+                     <div className="text-center flex-1 btn btn-outline">
+                       PROCESADO 
+                    </div>
+                </div>
+            </div>
+            <div className="w-1/6 divider divider-horizontal divider-success">15:30</div>
+        </div>
+    </div>)
 }
 
 export default EnamelDeliveries;
--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Home() {
-    return <div className="flex flex-col">
+    return <div className="flex flex-col max-w-72 mx-auto space-y-4 mt-4">
 
         <a href="/form" className="btn btn-primary">Pedidos</a>
         <a href="/deliveries" className="btn btn-primary">Entregas</a>
