7b8a1e1108170982fbab25a47c4ec2c842a1854c
andriyskb <148741323+andriyskb@users.noreply.github.com>
2024-04-23 12:11:30 +0000

    actualizado

--- src/Home.js
+++ src/Home.js
@@ -1,12 +1,8 @@
 import React from "react";
+import EnamelDeliveries from "./EnamelDeliveries";
 
 function Home() {
-    return <div className="flex flex-col max-w-72 mx-auto space-y-4 mt-4">
-
-        <a href="/form" className="btn btn-primary">Pedidos</a>
-        <a href="/deliveries" className="btn btn-primary">Entregas</a>
-
-    </div>
+    return <EnamelDeliveries />
 }
 
 export default Home;
